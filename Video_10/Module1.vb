f9e9600ba25768497ee14a4a5d85afb217a1692a
Gil Solis <gilsolisuw@gmail.com>
2022-02-02 18:00:35 -0700

    Video 10

---
+++ Video_10/Module1.vb
@@ -0,0 +1,96 @@
+﻿Module Module1
+
+    Sub Main()
+        'Dim myDouble As Double = 11
+        'Console.WriteLine("Initial value: 11 New Value:" & myDouble)
+        'myDouble += 23
+        'Console.WriteLine("Initial value: 11 New Value:" & myDouble)
+        'myDouble *= 23
+        ''Console.WriteLine("Initial value: 11 New Value:" & myDouble)
+        'Console.ReadLine()
+
+        'Dim userName As String = Nothing
+        'Dim userAge As Integer = Nothing
+        'Dim userSalary As Double = Nothing
+        'Console.WriteLine("What is your name?")
+        'userName = Console.ReadLine()
+        'Console.WriteLine("What is your age?")
+        'userAge = Console.ReadLine()
+        'Console.WriteLine("What is your salary?")
+        'userSalary = Console.ReadLine()
+        'Console.Write("Your name is:" & userName)
+        'Console.ReadLine()
+
+        'Console.WriteLine("Please enter the first number:")
+        'Dim num1 As Double = Console.ReadLine()
+        'Console.WriteLine("First number:" & num1)
+        'Console.WriteLine("Please enter the second number:")
+        'Dim num2 As Double = Console.ReadLine()
+        'Console.WriteLine("Second number:" & num2)
+        'Dim answer As Double = num1 * num2
+        'Console.WriteLine("Answer is " & answer)
+        'Console.ReadLine()
+
+        'Console.WriteLine("What is your name?")
+        'Dim userName As String = Console.ReadLine()
+        'Console.WriteLine("What is your password?")
+        'Dim password As String = Console.ReadLine()
+        'If userName = "Sam" Then
+        '    Console.WriteLine("Welcome Sam!")
+        'End If
+        'If password = "thenewboston" Then
+        '    Console.WriteLine("Correct")
+        'End If
+        'Console.ReadLine()
+
+        'Console.WriteLine("What is your name?")
+        'Dim userName As String = Console.ReadLine()
+        'Console.WriteLine("What is your password?")
+        'Dim password As String = Console.ReadLine()
+        'If userName = "Sam" Then
+        '    Console.WriteLine("Welcome Sam!")
+        'ElseIf userName = "Tim" Then
+        '    Console.WriteLine("Welcome Tim!")
+        'Else
+        '    Console.WriteLine("Not valid")
+        'End If
+        'Console.ReadLine()
+
+        'Dim Age As Integer = Nothing
+        'Dim hasInsurance As Boolean = Nothing
+        'Console.WriteLine("What is your Age?")
+        'Age = Console.ReadLine()
+        'Console.WriteLine("Do you have insurance?")
+        'hasInsurance = Console.ReadLine()
+        'If Age = 16 Then
+        '    If hasInsurance = True Then
+        '        Console.WriteLine("You can drive")
+        '    Else
+        '        Console.WriteLine("Probably shouldnt drive")
+
+        '    End If
+        'End If
+
+        'Console.ReadLine()
+        'Dim isInKindergarden As Boolean = Nothing
+        'Dim canDrink As Boolean = Nothing
+        'Dim isSeniorCitizen = Nothing
+        'Console.WriteLine("what is your age?")
+        'Dim age As Integer = Console.ReadLine()
+        'If age <> 5 Then
+        '    isInKindergarden = False
+        'Else
+        '    isInKindergarden = True
+        'End If
+        'If age >= 65 Then
+        '    isSeniorCitizen = True
+        'Else
+        '    isSeniorCitizen = False
+        'End If
+        'If age >= 21 Then
+        '    canDrink = True
+
+
+    End Sub
+
+End Module
